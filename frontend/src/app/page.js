34331c0b90d713ceec8574cd9e58536978b278e3
AdrianSzwaczyk <132113198+AdrianSzwaczyk@users.noreply.github.com>
2025-03-14 22:41:10 +0100

    Just starting

---
+++ frontend/src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="bg-red-500 text-white min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">TailwindCSS is Working! 🚀</h1>
+    </main>
+  );
+}
+
